85f2ca0f423bc6c50a1dbd2ea4655c59bde84000
Kevin Turcumani <kevinjaja98@gmail.com>
2019-05-07 11:54:46 +1000

    Class 1

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Prime31;
+
+[RequireComponent(typeof(CharacterController2D))]
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float gravity = -10f;
+
+    private CharacterController2D controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float inputH = Input.GetAxis("Horizontal");
+        float inputV = Input.GetAxis("Vertical");
+        controller.move(transform.right * inputH * moveSpeed * Time.deltaTime);
+    }
+}
